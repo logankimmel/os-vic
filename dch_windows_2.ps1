a5101327b7fd54fbd8fa9e56e3cf1b34caa9bd0e
Logan Kimmel <logan@lkimmel.com>
2018-04-16 12:08:04 -0500

    Add Windows Docker host to new cluster
    The second powershell script will take in arguments for Admiral
    It will use these arguments to make rest calls to the Admiral host
    to register itself with the Admiral endpoint, creating a new Cluster

--- dch_windows_2.ps1
+++ dch_windows_2.ps1
@@ -17,3 +17,50 @@ dockerd -H tcp://0.0.0.0:2375 --register-service
 New-NetFirewallRule -DisplayName 'Docker Inbound' -Profile @('Domain', 'Public', 'Private') -Direction Inbound -Action Allow -Protocol TCP -LocalPort 2375
 # Start the Docker service.
 Start-Service docker
+
+# Register the docker host to the Admiral endpoint
+If ($args.Length -eq 0) {
+    echo "No Admiral enpoint supplied. Will function as standalone Docker Host"
+    exit
+} Else {
+    $endpoint=$args[0]
+    $admiralAdmin=$args[1]
+    $admiralPass=$args[2]
+}
+
+$url = "http://" + $endpoint + ":8282/core/authn/basic"
+$Data = @{
+    requestType = "LOGIN"
+} | ConvertTo-Json
+$secpasswd = ConvertTo-SecureString $admiralPass -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ($admiralAdmin, $secpasswd)
+
+$res = Invoke-WebRequest -Method Post -Uri $url -Credential $Cred -ContentType "application/json" -Body $Data
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("x-xenon-auth-token", $res.Headers.'x-xenon-auth-token')
+$headers.Add("x-project", "/projects/default-project")
+
+$ip = Get-NetIPAddress -InterfaceAlias 'Ethernet0' -AddressFamily IPv4
+$addr = $ip.IPv4Address
+
+$url = "http://192.168.88.128:8282/resources/clusters?%24limit=50&expand=true&documentType=true&%24count=true"
+$res = Invoke-RestMethod -Method Get -Uri $url -ContentType "application/json" -Headers $headers
+
+If ($res.documentLinks.Length -eq 0) {
+  echo "No current clusters, adding host to Default Cluster"
+  $data = @{
+    hostState = @{
+      address = "http://" + $addr + ":2375"
+      customProperties = @{
+        __containerHostType = "DOCKER"
+        __adapterDockerType = "API"
+        __clusterName = "Default"
+      }
+    }
+    acceptCertificate = "false"
+  } | ConvertTo-Json
+  $url = "http://" + $endpoint + ":8282/resources/clusters"
+  $res = Invoke-RestMethod -Method Post -Uri $url -ContentType "application/json" -Body $Data -Headers $headers
+  echo "Successfully added new Docker host to Admiral cluster"
+}
